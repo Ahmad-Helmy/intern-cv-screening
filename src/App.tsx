b19c479644bc7fd915f1b45867a7c5aec9a2dd60
Naira Ehab <nairaehabb8@gmail.com>
2026-07-27 18:01:09 +0300

    "remove the buttonback.css file"

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import BackButton from "./UI/Atoms/TextButton/BackButton";
 function App() {
   return (
     <>
+<<<<<<< HEAD
       <div>
         <BackButton
           label="Back to candidates"
           onClick={() => alert("clicked!")}
         />
       </div>
+=======
+
+>>>>>>> 234a006 ("remove the buttonback.css file")
     </>
   );
 }
