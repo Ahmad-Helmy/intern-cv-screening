e6b208ad9028bc7d269a767c77eb6bc5b7e5160a
mohamed abdallah <mohamed234552@bue.edu.eg>
2026-07-27 17:19:04 +0300

    Input Field + CSS

---
+++ src/UI/Atoms/InputField/InputField.tsx
@@ -0,0 +1,20 @@
+function InputField({placeholder, name}) {
+    return (
+    <>
+    <label>{name}</label>
+
+    <input type="text"
+           name={name}
+           placeholder={placeholder}
+           id="InputField"/>
+    </>
+    )
+
+}
+
+export default InputField;
+
+
+
+
+
