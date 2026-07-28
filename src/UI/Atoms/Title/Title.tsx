b26460a7d509b1bcd11641ed97917bee8cb878f3
omar elbarbary <omar.m.elbarbary@gmail.com>
2026-07-28 11:16:49 +0300

    create infotitle component

--- src/UI/Atoms/Title/Title.tsx
+++ src/UI/Atoms/Title/Title.tsx
@@ -1,43 +1,48 @@
 import "./Title.css";
 
 function getClassName(type: string) {
-    switch (type) {
-        case "XXlarge":
-            return "title-XXlarge";
-        case "Xlarge":
-            return "title-Xlarge";
-        case "large":
-            return "title-large";
-        case "medium":
-            return "title-medium";
-        case "small":
-            return "title-small";
-        case "extra-small":
-            return "title-extra-small";
-        default:
-            return "title-medium";
-    }
+  switch (type) {
+    case "XXlarge":
+      return "title-XXlarge";
+    case "Xlarge":
+      return "title-Xlarge";
+    case "large":
+      return "title-large";
+    case "medium":
+      return "title-medium";
+    case "small":
+      return "title-small";
+    case "extra-small":
+      return "title-extra-small";
+    default:
+      return "title-medium";
+  }
 }
 
 function getVariantClassName(variant: string) {
-    switch (variant) {
-        case "primary":
-            return "variant-primary";
-        case "secondary":
-            return "variant-secondary";
-        case "muted":
-            return "variant-muted";
-        default:
-            return "variant-primary";
-    }
-}   
+  switch (variant) {
+    case "primary":
+      return "variant-primary";
+    case "secondary":
+      return "variant-secondary";
+    case "muted":
+      return "variant-muted";
+    default:
+      return "variant-primary";
+  }
+}
 
-export default function Title({type, children, variant}: {type: string; children: React.ReactNode; variant: string}) {
+export default function Title({
+  type,
+  children,
+  variant,
+}: {
+  type: string;
+  children: React.ReactNode;
+  variant: string;
+}) {
+  const className = getClassName(type);
+  const variantClassName = getVariantClassName(variant);
 
-    const className = getClassName(type);
-    const variantClassName = getVariantClassName(variant);
-
-    return <p className={`default ${className} ${variantClassName}`} >{children}</p>
-
-
+  return <p className={` ${className} ${variantClassName}`}>{children}</p>;
 }
