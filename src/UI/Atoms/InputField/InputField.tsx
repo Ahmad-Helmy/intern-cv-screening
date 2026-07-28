7b0c9f0d9c51721e7269e70e6e8802b2417481cf
Mariette <mariette.maged4@gmail.com>
2026-07-28 12:49:34 +0300

    "Added value and onChange to input field"

--- src/UI/Atoms/InputField/InputField.tsx
+++ src/UI/Atoms/InputField/InputField.tsx
@@ -1,18 +1,24 @@
 type InputFieldProps = {
-    placeholder: string;
-    name?: string;
+  placeholder?: string;
+  name: string;
+  value: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-function InputField({ placeholder, name }: InputFieldProps) {
-    return (
-        <>
-            <label>{name}</label>
+function InputField({ placeholder, name, value, onChange }: InputFieldProps) {
+  return (
+    <>
+      <label>{name}</label>
 
-            <input type="text"
-                name={name}
-                placeholder={placeholder}
-                id="InputField" />
-        </>
-    );
+      <input
+        type="text"
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        id="InputField"
+      />
+    </>
+  );
 }
 export default InputField;
