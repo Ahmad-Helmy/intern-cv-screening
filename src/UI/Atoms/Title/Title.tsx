ddb3427fa4b1764b77a2d20a88d49fcbbd9fcf5d
M-besheer <mbesheer26105@gmail.com>
2026-07-28 12:52:53 +0300

    refactored the title atom component

--- src/UI/Atoms/Title/Title.tsx
+++ src/UI/Atoms/Title/Title.tsx
@@ -1,43 +1,13 @@
 import "./Title.css";
 
-function getClassName(type: string) {
-    switch (type) {
-        case "XXlarge":
-            return "title-XXlarge";
-        case "Xlarge":
-            return "title-Xlarge";
-        case "large":
-            return "title-large";
-        case "medium":
-            return "title-medium";
-        case "small":
-            return "title-small";
-        case "extra-small":
-            return "title-extra-small";
-        default:
-            return "title-medium";
-    }
-}
+type TitleProps = {
+    type: 'xxlarge' | 'xlarge' | 'large' | 'medium' | 'small' | 'x-small';
+    children: React.ReactNode;
+    variant?: 'primary' | 'secondary' | 'muted';
+};
 
-function getVariantClassName(variant: string) {
-    switch (variant) {
-        case "primary":
-            return "variant-primary";
-        case "secondary":
-            return "variant-secondary";
-        case "muted":
-            return "variant-muted";
-        default:
-            return "variant-primary";
-    }
-}   
+export default function Title({type = 'medium', children, variant = 'primary'}: TitleProps) {
 
-export default function Title({type, children, variant}: {type: string; children: React.ReactNode; variant: string}) {
-
-    const className = getClassName(type);
-    const variantClassName = getVariantClassName(variant);
-
-    return <p className={`default ${className} ${variantClassName}`} >{children}</p>
-
+    return <p className={`default title-${type} variant-${variant}`} >{children}</p>
 
 }
