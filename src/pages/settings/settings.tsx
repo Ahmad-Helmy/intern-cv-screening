b65c159353e66ed7c8fba8e3fc76cc4bb6232cc1
Mohamed Eid <mohamed.eid@celfocus.com>
2026-07-29 12:46:14 +0300

    feat(settings-page): imported needed components to settings.tsx

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./settings.css";
 import DefaultTemplate from "../../UI/DefaultTemplates/DefaultTemplate";
+import Badge from "../../UI/Atoms/Badge/Badge.tsx";
+import Button from "../../UI/Atoms/Buttons/Button.tsx";
+import DropdownMenu from "../../UI/Atoms/DropdownMenu/DropdownMenu.tsx";
+import InputField from "../../UI/Atoms/InputField/InputField";
+import Title from "../../UI/Atoms/Title/Title";
+import Logo from "../../UI/Atoms/Logo/Logo";
+import CardInfo from "../../UI/Molecules/CardInfo/CardInfo";
+import InfoTitle from "../../UI/Molecules/InfoTitle/InfoTitle.tsx";
+
 const Settings: React.FC = () => {
   return (
     <DefaultTemplate>
