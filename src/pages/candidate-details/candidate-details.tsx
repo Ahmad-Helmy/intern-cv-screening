fcb21aec76076c2c082e620e332ec9e031ab3004
mohamed abdallah <mohamed234552@bue.edu.eg>
2026-07-29 13:11:32 +0300

    feat(candidate-details): main container added

--- src/pages/candidate-details/candidate-details.tsx
+++ src/pages/candidate-details/candidate-details.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./candidate-details.css";
 import DefaultTemplate from "../../UI/DefaultTemplates/DefaultTemplate";
+import CardInfo from "../../UI/Molecules/CardInfo/CardInfo";
+import Title from "../../UI/Atoms/Title/Title";
 
 const CandidateDetails: React.FC = () => {
   return (
     <DefaultTemplate>
-      <div className="candidate-details">Candidate Details</div>
+      <CardInfo title="Mohamed Khalil">
+        <Title type="small" variant="primary">
+          mohammed.khalil@university.edu . +351 942 318 705 . Full-Stack
+          Nominated Video: Pass Strongly Recommended{" "}
+        </Title>
+      </CardInfo>
     </DefaultTemplate>
   );
 };
