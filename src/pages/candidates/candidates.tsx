0778d439545a77a1be9597ff56e4eecbef7c404d
omar elbarbary <omar.m.elbarbary@gmail.com>
2026-07-29 12:51:17 +0300

    feat(CandidateDetails, Candidates): create candidate details and candidates pages with styling

---
+++ src/pages/candidates/candidates.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./candidates.css";
+import DefaultTemplate from "../../UI/DefaultTemplates/DefaultTemplate";
+
+const Candidates: React.FC = () => {
+  return (
+    <DefaultTemplate>
+      <div className="candidates">Candidates</div>
+    </DefaultTemplate>
+  );
+};
+
+export default Candidates;
