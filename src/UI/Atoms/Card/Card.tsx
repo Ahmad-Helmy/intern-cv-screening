a5530fbafbadfe908902ede2643a0befb59ae1b1
Mazin Bersy <nb31229@novabase.pt>
2026-07-29 15:51:36 +0300

    refactor: added table variant for card

--- src/UI/Atoms/Card/Card.tsx
+++ src/UI/Atoms/Card/Card.tsx
@@ -1,7 +1,13 @@
 import "./Card.css";
 
-const Card = ({ children }: { children: React.ReactNode }) => {
-  return <div className="card">{children}</div>;
+const Card = ({
+  children,
+  table = false,
+}: {
+  children: React.ReactNode;
+  table?: boolean;
+}) => {
+  return <div className={`card ${table ? "table-card" : ""}`}>{children}</div>;
 };
 
 export default Card;
