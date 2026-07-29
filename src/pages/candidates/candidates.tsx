4247e3d54f700201c5ed7575c1767b910ae0a8bd
Mennatullah Ali <mennatullah.ali+celfocus@celfocus.com>
2026-07-29 17:05:09 +0300

    feat(candidates-page): added table header and page title

--- src/pages/candidates/candidates.tsx
+++ src/pages/candidates/candidates.tsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./candidates.css";
 import DefaultTemplate from "../../UI/DefaultTemplates/DefaultTemplate";
 import InfoTitle from "../../UI/Molecules/InfoTitle/InfoTitle";
+import Title from "../../UI/Atoms/Title/Title";
+import DropdownMenu from "../../UI/Atoms/DropdownMenu/DropdownMenu";
+import CardInfo from "../../UI/Molecules/CardInfo/CardInfo";
+import InputField from "../../UI/Atoms/InputField/InputField";
+import Badge from "../../UI/Atoms/Badge/Badge";
 
 const Candidates: React.FC = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedInternship, setSelectedInternship] = useState("");
+
+  const getCardTitle = () => {
+    if (!selectedInternship) {
+      return null;
+    }
+    return (
+      <div className="candidates-header">
+        <div className="candidates-header-title">
+          <Title type="medium" variant="primary">
+            {selectedInternship}
+          </Title>
+          <Badge type="evaluated" text={"90" + " candidates"} />
+        </div>
+
+        <div className="candidates-actions">
+          <div className="">
+            {
+              <InputField
+                placeholder="Search candidates..."
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+            }
+          </div>
+          <div className="">
+            {
+              <DropdownMenu
+                size="large"
+                options={[
+                  "All Statuses",
+                  "Nominated",
+                  "Evaluated",
+                  "Rejected",
+                  "Processing",
+                  "Imported",
+                ]}
+                onChange={(value) => {
+                  console.log(value);
+                }}
+              />
+            }
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <DefaultTemplate>
       <div className="candidates">
@@ -11,6 +64,25 @@ const Candidates: React.FC = () => {
           label="Candidates"
           value="Select an internship to review its applicants"
         />
+        <div className="label-dropdown">
+          <Title type="small" variant="primary">
+            Internship
+          </Title>
+          <DropdownMenu
+            size="large"
+            options={[
+              "Software Engineering Summer Internship 2026",
+              "Data Science Internship 2026",
+              "Cloud & DevOps Internship 2026",
+            ]}
+            onChange={(value) => {
+              setSelectedInternship(value);
+            }}
+          />
+        </div>
+        <CardInfo title={getCardTitle()}>
+          <div className="candidates__table">{/* Table component */}</div>
+        </CardInfo>
       </div>
     </DefaultTemplate>
   );
