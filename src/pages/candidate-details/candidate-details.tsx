6f0d8ea871c7fc638807c0b7257332296565d11d
mohamed abdallah <mohamed234552@bue.edu.eg>
2026-07-29 13:28:41 +0300

    feat(candidate-details): main container fix

--- src/pages/candidate-details/candidate-details.tsx
+++ src/pages/candidate-details/candidate-details.tsx
@@ -7,12 +7,14 @@ import Title from "../../UI/Atoms/Title/Title";
 const CandidateDetails: React.FC = () => {
   return (
     <DefaultTemplate>
-      <CardInfo title="Mohamed Khalil">
-        <Title type="small" variant="primary">
-          mohammed.khalil@university.edu . +351 942 318 705 . Full-Stack
-          Nominated Video: Pass Strongly Recommended{" "}
-        </Title>
-      </CardInfo>
+      <div className="details-conatiner">
+        <CardInfo title="Mohamed Khalil">
+          <Title type="small" variant="primary">
+            mohammed.khalil@university.edu . +351 942 318 705 . Full-Stack
+            Nominated Video: Pass Strongly Recommended{" "}
+          </Title>
+        </CardInfo>
+      </div>
     </DefaultTemplate>
   );
 };
