790b864c261b80747748d65ad8396c06996f7b8f
mohamed abdallah <mohamed234552@bue.edu.eg>
2026-07-30 12:10:46 +0300

    fix(create-routes): add login, candidates, candidates details, and settings routes, and added the deault template function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import "./App.css";
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router";
 import CandidateDetails from "./pages/candidate-details/candidate-details";
+import Candidates from "./pages/candidates/candidates";
+import LoginPage from "./pages/login/Login";
+import Settings from "./pages/settings/settings";
+import PageTemplate from "./UI/DefaultTemplates/DefaultTemplate";
+
+function DefaultTemplate() {
+  return (
+    <PageTemplate>
+      <Outlet />
+    </PageTemplate>
+  );
+}
 
 function App() {
   return (
-    <>
-      <CandidateDetails />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route element={<ProtectedRoute />}> */}
+        {/* Auth gate */}
+        <Route element={<DefaultTemplate />}>
+          {/* Sidebar + main */}
+          <Route index element={<Navigate to="/candidates" replace />} />
+          <Route path="candidates" element={<Candidates />} />
+          <Route path="candidates/:id" element={<CandidateDetails />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        {/* </Route> */}
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
