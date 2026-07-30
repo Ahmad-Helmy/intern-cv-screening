e78d849ac9722c3c56ebc7d3e8c4897c0c267447
NB31219 <NB31219@novabase.pt>
2026-07-30 15:49:49 -0700

    feat(protected-route-component): create the component

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,14 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      {/* <Route element={<ProtectedRoute />}> */}
-      {/* Auth gate */}
-      <Route element={<PageTemplate />}>
-        {/* Sidebar + main */}
-        <Route index element={<Navigate to="/candidates" replace />} />
-        <Route path="candidates" element={<Candidates />} />
-        <Route path="candidates/:id" element={<CandidateDetails />} />
-        <Route path="settings" element={<Settings />} />
-        {/* </Route> */}
+      <Route element={<ProtectedRoute />}>
+        <Route element={<PageTemplate />}>
+          <Route index element={<Navigate to="/candidates" replace />} />
+          <Route path="candidates" element={<Candidates />} />
+          <Route path="candidates/:id" element={<CandidateDetails />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Route>
-
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
