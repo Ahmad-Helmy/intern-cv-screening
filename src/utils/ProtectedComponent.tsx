c9d745ae25fda4412dd2ba095860cb64f67acb7a
M-besheer <mbesheer26105@gmail.com>
2026-07-30 16:36:07 +0300

    feat(protectedroute): added mock users

--- src/utils/ProtectedComponent.tsx
+++ src/utils/ProtectedComponent.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from "react-router";
 // import { useAuth } from "../hooks/useAuth";
-
 export default function ProtectedRoute() {
   const { isAuthenticated } = useAuth();
 
