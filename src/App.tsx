e9501093014895c89bc3014e2f1c19dcd127beff
mohamed abdallah <mohamed234552@bue.edu.eg>
2026-07-30 12:22:08 +0300

    fix(create-routes): in the app.tsx tags closings

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import "./App.css";
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router";
+import { Navigate, Outlet, Route, Routes } from "react-router";
 import CandidateDetails from "./pages/candidate-details/candidate-details";
 import Candidates from "./pages/candidates/candidates";
 import LoginPage from "./pages/login/Login";
 import Settings from "./pages/settings/settings";
 import PageTemplate from "./UI/DefaultTemplates/DefaultTemplate";
 
-function DefaultTemplate() {
-  return (
-    <PageTemplate>
-      <Outlet />
-    </PageTemplate>
-  );
-}
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        {/* <Route element={<ProtectedRoute />}> */}
-        {/* Auth gate */}
-        <Route element={<DefaultTemplate />}>
-          {/* Sidebar + main */}
-          <Route index element={<Navigate to="/candidates" replace />} />
-          <Route path="candidates" element={<Candidates />} />
-          <Route path="candidates/:id" element={<CandidateDetails />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-        {/* </Route> */}
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      {/* <Route element={<ProtectedRoute />}> */}
+      {/* Auth gate */}
+      <Route
+        element={
+          <PageTemplate>
+            <Outlet />
+          </PageTemplate>
+        }
+      >
+        {/* Sidebar + main */}
+        <Route index element={<Navigate to="/candidates" replace />} />
+        <Route path="candidates" element={<Candidates />} />
+        <Route path="candidates/:id" element={<CandidateDetails />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+      {/* </Route> */}
 
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
