d7cd796a7404426b810c28fe79cf1b2c3330a320
Mariette <mariette.maged4@gmail.com>
2026-08-02 16:27:43 +0300

    feat(candidates):fetch-internships

--- src/pages/candidates/candidates.tsx
+++ src/pages/candidates/candidates.tsx
@@ -1,7 +1,7 @@
 import "./candidates.css";
 import InfoTitle from "../../UI/Molecules/InfoTitle/InfoTitle";
 import Table from "../../UI/Molecules/Table/Table";
-import { candidateColumns, candidateData } from "./mockData";
+import { candidateColumns } from "./mockData";
 import { mapCandidatesToRows } from "./candidatesService";
 import CardInfo from "../../UI/Molecules/CardInfo/CardInfo";
 import Title from "../../UI/Atoms/Title/Title";
@@ -10,8 +10,13 @@ import InputField from "../../UI/Atoms/InputField/InputField";
 import Badge from "../../UI/Atoms/Badge/Badge";
 import { useNavigate, useSearchParams } from "react-router";
 import { getCandidates } from "../../services/candidates";
-import { useState } from "react";
-import { type CandidateStatus } from "../../types/api/candidates";
+import { getInternships } from "../../services/internships";
+import { useEffect, useState } from "react";
+import type {
+  CandidateStatus,
+  CandidateListItem,
+} from "../../types/api/candidates";
+import type { InternshipListItem } from "../../types/api/internships";
 
 const Candidates = () => {
   const navigate = useNavigate();
@@ -20,6 +25,18 @@ const Candidates = () => {
     CandidateStatus | undefined
   >(undefined);
   const [selectedInternship, setSelectedInternship] = useState("");
+  const [internships, setInternships] = useState<InternshipListItem[]>([]);
+  const [candidates, setCandidates] = useState<CandidateListItem[]>([]);
+
+  useEffect(() => {
+    async function initializeInternships() {
+      const data = await getInternships();
+      console.log(data);
+
+      setInternships(data);
+    }
+    initializeInternships();
+  }, []);
 
   const handleGetCandidates = async ({
     id,
@@ -34,18 +51,8 @@ const Candidates = () => {
       search: search || searchValue,
       status: status || selectedStatus,
     });
-    return data;
+    setCandidates(data);
   };
-
-  const rows = mapCandidatesToRows(
-    candidateData,
-    {
-      search: searchValue,
-      internship: selectedInternship,
-      status: selectedStatus,
-    },
-    (id: string) => navigate(`/candidates/${id}`),
-  );
 
   const getCardTitle = () => {
     if (!selectedInternship) {
@@ -57,7 +64,7 @@ const Candidates = () => {
           <Title type="medium" variant="primary">
             {selectedInternship}
           </Title>
-          <Badge type="evaluated" text={rows.length + " candidates"} />
+          <Badge type="evaluated" text={candidates.length + " candidates"} />
         </div>
 
         <div className="candidates-actions">
@@ -112,20 +119,10 @@ const Candidates = () => {
         </Title>
         <DropdownMenu
           size="large"
-          options={[
-            {
-              id: "Software Engineering Summer Internship 2026",
-              label: "Software Engineering Summer Internship 2026",
-            },
-            {
-              id: "Data Science Internship 2026",
-              label: "Data Science Internship 2026",
-            },
-            {
-              id: "Cloud & DevOps Internship 2026",
-              label: "Cloud & DevOps Internship 2026",
-            },
-          ]}
+          options={internships.map((internship) => ({
+            id: internship.id,
+            label: internship.name,
+          }))}
           selectedOption={selectedInternship}
           onChange={(value) => {
             setSelectedInternship(value);
@@ -137,7 +134,7 @@ const Candidates = () => {
         <div className="candidates-table">
           <Table
             columns={candidateColumns}
-            data={selectedInternship ? rows : []}
+            data={selectedInternship ? candidates : []}
           />
         </div>
       </CardInfo>
